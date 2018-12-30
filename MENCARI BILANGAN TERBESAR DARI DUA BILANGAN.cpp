3a8ca2efbcaff7f4ee270daf06ca1e308d71b95f
Daniel Xiha <40915588+HazlerDaniel@users.noreply.github.com>
2018-12-30 18:03:11 +0700

    Add files via upload

---
+++ MENCARI BILANGAN TERBESAR DARI DUA BILANGAN.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+main(){
+	
+	int a,b;
+	printf("\nbilangan 1=");
+	scanf("%i",&a);
+	printf("\nbilangan 2=");
+	scanf("%i",&b);
+	
+	if(a>b)
+	printf("bilangan yang terbesar adalah = %i",a);
+	else
+	printf("bilangan yang terbesar adalah= %i",b);
+}
